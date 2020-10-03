1dbfd6a62c6e24fb07eac0553333e24ff5c16b1b
Takashi Kawachi <tkawachi@gmail.com>
2020-10-04 00:34:30 +0900

    Used other way to send henkan/muhenkan
    https://okapies.hateblo.jp/entry/2019/02/02/133953

--- keyboards/xd75/keymaps/kawachi/keymap.c
+++ keyboards/xd75/keymaps/kawachi/keymap.c
@@ -17,60 +17,100 @@
 
 #include QMK_KEYBOARD_H
 
-// https://leopardgecko.hatenablog.com/entry/2017/11/09/124103
-enum user_macro {
-  UM_EMHL, // Eisu MuHenkan Lower
-  UM_KHKR  // Kana HenKan Raise
-};
-
-#define M_EMHL MACROTAP(UM_EMHL)
-#define M_KHKR MACROTAP(UM_KHKR)
-
 
 // Layer shorthand
 enum LAYER{
-  _QWERTY,
-  _RAISE,
-  _LOWER
+  _QW,
+  _RS,
+  _LW
+};
+
+enum custom_keycords {
+  LOWER = SAFE_RANGE,
+  RAISE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  //qwerty layer(almost use default)
- [_QWERTY] = LAYOUT(
+ [_QW] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ENT,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,  KC_HOME, KC_PGUP,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  KC_BSLS, KC_END,  KC_PGDN,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, KC_RSFT, KC_UP,   KC_MSTP,
-  KC_ESC,  KC_SPC,  KC_LALT, KC_LGUI, M_EMHL,  KC_SPC,  KC_SPC,  M_KHKR,  KC_RGUI, KC_RALT, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT
+  KC_ESC,  KC_SPC,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT
  ),
 //LOWER
- [_LOWER] = LAYOUT(
+ [_LW] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, RESET,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  KC_TRNS, KC_PAUS, _______, _______,
   KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, _______, KC_MINS, KC_LPRN, KC_RPRN, KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, _______, KC_UNDS, _______, _______, KC_BSLS, KC_BSLS, KC_TRNS, KC_VOLU, KC_TRNS,
-  KC_TRNS, AG_SWAP, KC_TRNS, KC_TRNS, _______, _______, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
+  KC_TRNS, AG_SWAP, KC_TRNS, KC_TRNS, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
  ),
 
 //RAISE
- [_RAISE] = LAYOUT(
+ [_RS] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  _______, _______, _______,
   KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_LCBR, KC_RCBR, _______, KC_TRNS, _______, _______, _______,
   KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LBRC, KC_RBRC, _______, _______, _______, KC_TRNS, _______,
-  KC_TRNS, AG_NORM, KC_TRNS, KC_TRNS, _______, KC_TRNS, KC_TRNS, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, AG_NORM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
  )
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-    switch(id) {
-        case UM_EMHL: // タップで「英数」と「無変換」、ホールドで「Lower」
-        return MACRO_TAP_HOLD_LAYER( record, MACRO(T(MHEN), T(LANG2), END), _LOWER );
-        case UM_KHKR: // タップで「かな」と「変換」、ホールドで「Raise」
-        return MACRO_TAP_HOLD_LAYER( record, MACRO(T(HENK), T(LANG1), END), _RAISE );
-        };
-        return MACRO_NONE;
+static bool lower_pressed = false;
+static uint16_t lower_pressed_time = 0;
+static bool raise_pressed = false;
+static uint16_t raise_pressed_time = 0;
+
+#define TAPPING_TERM 200
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LOWER:
+      if (record->event.pressed) {
+        lower_pressed = true;
+        lower_pressed_time = record->event.time;
+        layer_on(_LW);
+      } else {
+        layer_off(_LW);
+
+        if (lower_pressed && (TIMER_DIFF_16(record->event.time, lower_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG2);
+          unregister_code(KC_LANG2);
+          register_code(KC_MHEN);
+          unregister_code(KC_MHEN);
+        }
+        lower_pressed = false;
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        raise_pressed = true;
+        raise_pressed_time = record->event.time;
+        layer_on(_RS);
+      } else {
+        layer_off(_RS);
+
+        if (raise_pressed && (TIMER_DIFF_16(record->event.time, raise_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG1);
+          unregister_code(KC_LANG1);
+          register_code(KC_HENK);
+          unregister_code(KC_HENK);
+        }
+        raise_pressed = false;
+      }
+      return false;
+      break;
+    default:
+      if (record->event.pressed) {
+        lower_pressed = false;
+        raise_pressed = false;
+      }
+      break;
+  }
+  return true;
 }
