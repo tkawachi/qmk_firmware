6b6feedc95ed198328bde581c2b051f10eacc8c6
Takashi Kawachi <tkawachi@gmail.com>
2020-10-05 21:59:37 +0900

    add backspace

--- keyboards/xd75/keymaps/kawachi/keymap.c
+++ keyboards/xd75/keymaps/kawachi/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   //qwerty layer(almost use default)
   [_QW] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, TG(_NUM),_______, KC_ENT,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, TG(_NUM),KC_BSPC, KC_ENT,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_WH_U, KC_HOME, KC_PGUP,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  KC_WH_D, KC_END,  KC_PGDN,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, KC_WBAK, KC_UP,   KC_WFWD,
