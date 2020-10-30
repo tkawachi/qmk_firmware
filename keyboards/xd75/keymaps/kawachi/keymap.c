fea7758e4ef203330c0daa98a17d606862eb0de8
Takashi Kawachi <tkawachi@gmail.com>
2020-10-31 01:05:45 +0900

    update keybind

--- keyboards/xd75/keymaps/kawachi/keymap.c
+++ keyboards/xd75/keymaps/kawachi/keymap.c
@@ -37,15 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //qwerty layer(almost use default)
   [_QW] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, TG(_NUM),KC_BSPC, KC_ENT,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_WH_U, KC_HOME, KC_PGUP,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  KC_WH_D, KC_END,  KC_PGDN,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BTN1, KC_HOME, KC_PGUP,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  KC_BTN2, KC_END,  KC_PGDN,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, KC_WBAK, KC_UP,   KC_WFWD,
     KC_ESC,  KC_SPC,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   //LOWER
   [_LW] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, RESET,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  KC_ACL0, KC_PAUS, _______, _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  KC_ACL0, _______, _______, _______,
     _______, _______, _______, KC_MS_U, _______, _______, _______, KC_MINS, KC_LPRN, KC_RPRN, KC_PIPE, KC_BTN1, _______, _______, _______,
     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_UNDS, _______, _______, KC_BSLS, KC_BTN2, _______, KC_VOLU, _______,
     _______, AG_SWAP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
